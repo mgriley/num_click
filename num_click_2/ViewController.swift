52fdecf504501f6da4802c1b3e3d6bb917984ecd
Matthew Riley <mgriley97@gmail.com>
2017-11-15 21:04:58 -0500

    Initial Commit

---
+++ num_click_2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  num_click_2
+//
+//  Created by Matthew Riley on 2017-11-15.
+//  Copyright © 2017 Matthew Riley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
